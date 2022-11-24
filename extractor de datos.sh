f0d802c526f68e858693233a18055f8631101b68
Miguel Gonzalez <47222395+miguelgh65@users.noreply.github.com>
2022-11-24 03:17:28 +0100

    First commit
    First commit

---
+++ extractor de datos.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWFkIl0sImV4cCI6MTY2OTA5NTI4MywiYXV0aG9yaXRpZXMiOlsiUk9MRV9QVUJMSUMiXSwianRpIjoiODMyMzdmYTAtMTdiNS00ZGIxLWFlZDYtMzlmMDQyYjkwYWNiIiwiY2xpZW50X2lkIjoiZG04dmVucWxsZ3FmcDRpb2NlZG1ianl4YjYzdXNpMWUifQ.dWzSbmGhf9BDuoCrFyaRWxyFZydg-pggI3pjX5FR1xk " -H "Content-Type: multipart/form-data;" -F "maxItems=50" -F "numPage=13" -F "center=40.3029200,-3.4037100" -F "propertyType=homes" -F "distance=15000" -F "operation=sale" "https://api.idealista.com/3.5/es/search">Madrid13.json
+
+
